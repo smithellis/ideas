ba6fcceb62ff5c7803a73ec3d04f7f68c2b22865
Smith Ellis <smithe@gmail.com>
2018-08-24 17:11:02 -0400

    Updates abound

--- dev_startup.py
+++ dev_startup.py
@@ -4,6 +4,8 @@ from ideas import db
 
 os.environ['FLASK_APP']='ideas.py'
 os.environ['FLASK_DEBUG']='1'
+
 db.drop_all()
 db.create_all()
+
 call(['flask','run'])
--- ideas.py
+++ ideas.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -8,26 +9,30 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
 from flask_user import roles_required
+from flask_login import current_user
+from flask_admin import Admin
+from flask_admin import BaseView, expose
+from flask_admin.contrib.sqla import ModelView
 
 # Use a Class-based config to avoid needing a 2nd file - terrible idea to fix later
 # os.getenv() enables configuration through OS environment variables
 
 class ConfigClass(object):
     # Flask settings
-	SECRET_KEY =              os.getenv('SECRET_KEY',       'THIS IS AN INSECURE SECRET')
-	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',     'sqlite:///single_file_app.sqlite')
-	CSRF_ENABLED = True
+    SECRET_KEY =              os.getenv('SECRET_KEY',       'THIS IS AN INSECURE SECRET')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',     'sqlite:///single_file_app.sqlite')
+    CSRF_ENABLED = True
     # Flask-Mail settings
-	MAIL_USERNAME =           os.getenv('MAIL_USERNAME',        '364142f32f4e55')
-	MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD',        '3bce037966e65a')
-	MAIL_DEFAULT_SENDER =     os.getenv('MAIL_DEFAULT_SENDER',  '"Ideas" <noreply@example.com>')
-	MAIL_SERVER =             os.getenv('MAIL_SERVER',          'smtp.mailtrap.io')
-	MAIL_PORT =           int(os.getenv('MAIL_PORT',            '2525'))
+    MAIL_USERNAME =           os.getenv('MAIL_USERNAME',        '364142f32f4e55')
+    MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD',        '3bce037966e65a')
+    MAIL_DEFAULT_SENDER =     os.getenv('MAIL_DEFAULT_SENDER',  '"Ideas" <noreply@example.com>')
+    MAIL_SERVER =             os.getenv('MAIL_SERVER',          'smtp.mailtrap.io')
+    MAIL_PORT =           int(os.getenv('MAIL_PORT',            '2525'))
 
-	# Flask-User settings
-	USER_APP_NAME        = "Ideas"                # Used by email templates
-	# SQLAlchemy Defaults to garbage and complaining, let's stop it
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Flask-User settings
+    USER_APP_NAME        = "Ideas"                # Used by email templates
+    # SQLAlchemy Defaults to garbage and complaining, let's stop it
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 #Define the app and the db
@@ -37,6 +42,7 @@ app.config.from_object(__name__+'.ConfigClass')
 
 try: app.config.from_object('local_settings')
 except: pass
+
 
 mail=Mail(app)
 db=SQLAlchemy(app)
@@ -64,6 +70,9 @@ class Role(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), unique=True)
 
+    def __repr__(self):
+        return '%s' % (self.name)
+
 # Define the UserRoles data model
 class UserRoles(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -71,95 +80,109 @@ class UserRoles(db.Model):
     role_id = db.Column(db.Integer(), db.ForeignKey('role.id', ondelete='CASCADE'))
 
 class Idea(db.Model):
-	id=db.Column(db.Integer,primary_key=True)
-	title=db.Column(db.String(80))
-	description=db.Column(db.Text)
-	fullname=db.Column(db.String(80))
-	email=db.Column(db.String(80))
-	department=db.Column(db.String(80))
-	total_votes=db.Column(db.Integer)
-	accepted=db.Column(db.Boolean, default=False, nullable=False)
-	votes=db.relationship('Vote',backref='idea',lazy=True)
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(80))
+    description=db.Column(db.Text)
+    fullname=db.Column(db.String(80))
+    email=db.Column(db.String(80))
+    department=db.Column(db.String(80))
+    total_votes=db.Column(db.Integer)
+    accepted=db.Column(db.Boolean, default=False, nullable=False)
+    votes=db.relationship('Vote',backref='idea',lazy=True)
 
-	def __repr__(self):
-		return '<Idea %r>' % self.title
+    def __repr__(self):
+        return '<Idea %r>' % self.title
 
 class Vote(db.Model):
-	id=db.Column(db.Integer,primary_key=True)
-	ipaddress=db.Column(db.String(80),nullable=False)
-	vote_state=db.Column(db.Integer,nullable=False)
-	idea_id=db.Column(db.Integer,db.ForeignKey('idea.id'),nullable=False)
+    id=db.Column(db.Integer,primary_key=True)
+    ipaddress=db.Column(db.String(80),nullable=False)
+    userid=db.Column(db.Integer)
+    vote_state=db.Column(db.Integer,nullable=False)
+    idea_id=db.Column(db.Integer,db.ForeignKey('idea.id'),nullable=False)
 
 # Setup Flask-User
 db_adapter = SQLAlchemyAdapter(db, User)        # Register the User model
 user_manager = UserManager(db_adapter, app)     # Initialize Flask-User
+
+# Hanky panky to protect the admin area
+class MyView(ModelView):
+    @expose('/')
+    @login_required
+    def index(self):
+        return self.render('/admin')
 
 # Reset the whole DATABASE
 db.drop_all()
 db.create_all()
 
 # Create 'user007' user with 'secret' and 'agent' roles
-if not User.query.filter(User.username=='user007').first():
-	user1 = User(username='user007', email='user007@example.com', active=True,password=user_manager.hash_password('Password1'))
-	user1.roles.append(Role(name='secret'))
-	user1.roles.append(Role(name='agent'))
-	db.session.add(user1)
-	db.session.commit()
+if not User.query.filter(User.username=='demo').first():
+    user1 = User(username='demo', email='smithe@gmail.com', active=True, confirmed_at=datetime.datetime.now(), password=user_manager.hash_password('Password1'))
+    user1.roles.append(Role(name='secret'))
+    user1.roles.append(Role(name='agent'))
+    db.session.add(user1)
+    db.session.commit()
 
+# Flask Admin Setup
+admin=Admin(app, name="Ideas", template_mode="bootstrap3")
+admin.add_view(MyView(User, db.session))
+admin.add_view(MyView(Idea, db.session))
 
 """ Define the routes and mechanics """
 
 @app.route('/', methods=['GET','POST'])
 def home():
-	if request.method=='POST':
-		title = request.form['title']
-		idea = request.form['idea']
-		fullname = request.form['fullname']
-		email = request.form['email']
-		department = request.form['department']
-		total_votes = 0
-		newidea = Idea(title=title,description=idea,fullname=fullname,email=email,department=department,total_votes=0,accepted=False)
-		db.session.add(newidea)
-		db.session.commit()
-		flash('Idea added!  Thanks!')
-	return render_template('index.html')
+    if request.method=='POST':
+        title = request.form['title']
+        idea = request.form['idea']
+        fullname = request.form['fullname']
+        email = request.form['email']
+        department = request.form['department']
+        total_votes = 0
+        newidea = Idea(title=title,description=idea,fullname=fullname,email=email,department=department,total_votes=0,accepted=False)
+        db.session.add(newidea)
+        db.session.commit()
+        flash('Got it!  We have added your idea.  An administrator will take a look, and if it appears authentic, it will get approved and become available for the voting process.  This step happens so we can avoid spam - it is difficult to allow anonymous submissions without getting a bit of spam.')
+    return render_template('index.html')
 
 @app.route('/active-ideas')
-@login_required
 def activeIdeas():
-	return render_template('active-ideas.html')
+    return render_template('active-ideas.html')
 
 @app.route('/faq')
 def faq():
-	return render_template('faq.html')
+    return render_template('faq.html')
 
 @app.route('/feedback')
 def feedback():
-	return render_template('feedback.html')
+    return render_template('feedback.html')
 
 @app.route('/vote', methods=['GET','POST'])
+@login_required
 def vote():
-	ipaddr = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-	all_ideas = Idea.query.all()
+    ipaddr = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    userid = int(current_user.get_id())
+    all_ideas = Idea.query.all()
+    for idea in all_ideas:
+        for vote in idea.votes:
+            if vote.userid == userid:
+                idea.voted="yes"
 
-	for idea in all_ideas:
-		for vote in idea.votes:
-			if vote.ipaddress == ipaddr:
-				idea.voted="yes"
-
-	return render_template('vote.html', all_ideas = all_ideas, ipaddress=ipaddr)
+    return render_template('vote.html', all_ideas = all_ideas, userid=userid, ipaddress=ipaddr)
 
 @app.route('/makevote',)
+@login_required
 def makevote():
-	ipaddr = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-	idea_id = request.args.get('idea_id',0,type=int)
-	vote_state = 1
-	vote = Vote(ipaddress=ipaddr,idea_id=idea_id, vote_state=vote_state)
-	thisidea = Idea.query.filter_by(id=idea_id).first()
-	if thisidea.total_votes == None:
-		thisidea.total_votes = 0
-	thisidea.total_votes = thisidea.total_votes+1
-	totalvotes = thisidea.total_votes
-	db.session.add(vote)
-	db.session.commit()
-	return jsonify(result=idea_id, totalvotes=totalvotes)
+    ipaddr = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    userid = int(current_user.get_id())
+    idea_id = request.args.get('idea_id',0,type=int)
+    vote_state = 1
+    vote = Vote(ipaddress=ipaddr, userid=userid, idea_id=idea_id, vote_state=vote_state)
+    thisidea = Idea.query.filter_by(id=idea_id).first()
+    if thisidea.total_votes == None:
+        thisidea.total_votes = 0
+    thisidea.total_votes = thisidea.total_votes+1
+    totalvotes = thisidea.total_votes
+    db.session.add(vote)
+    db.session.commit()
+    return jsonify(result=idea_id, totalvotes=totalvotes)
